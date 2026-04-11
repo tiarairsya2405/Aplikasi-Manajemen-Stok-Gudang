48b2590b2b5768ec60f1223a0182ad4a674515e4
TIARA IRSYA <tiarairsya2405@gmail.com>
2026-04-11 18:58:26 +0700

    view dan controller

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,33 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\AdminController;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
+// Route default (home)
 Route::get('/', [HomeController::class, 'index']);
 Route::get('/contact', [HomeController::class, 'contact']);
 
+// Route view biasa
 Route::get('/welcome', function () {
     return view('welcome');
+});
+
+// Route parameter
+Route::get('/user/{id}', function ($id) {
+    return 'User dengan ID ' . $id;
+});
+
+// Group route admin
+Route::prefix('admin')->group(function () {
+
+    Route::get('/dashboard', function () {
+        return 'Admin Dashboard';
+    });
+
+    Route::get('/users', function () {
+        return 'Admin Users';
+    });
+
+    // ✅ Route ke controller AdminController
+    Route::get('/barang', [AdminController::class, 'tampilkan']);
 });
